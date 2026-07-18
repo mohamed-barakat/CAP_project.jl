0e6fab9bdbb404d3fc1fa0208f84ec6bf0684123
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2026-07-18 10:01:55 +0200

    Update to GAP's Toposes v2026.07-01

--- Toposes/src/read.g.autogen.jl
+++ Toposes/src/read.g.autogen.jl
@@ -15,8 +15,11 @@ include( "gap/ToposDerivedMethods.semiautogen.gi.autogen.jl" );
 ## Category of relations
 include( "gap/CategoryOfRelations.gi.autogen.jl" );
 
-## Tools
-include( "gap/Tools.gi.autogen.jl" );
+#= comment for Julia
+if IsPackageMarkedForLoading( "Digraphs", ">= 1.3.1" ) then
+    include( "gap/ToolsUsingDigraphs.gi.autogen.jl" );
+fi;
+# =#
 
 ## DPO
 include( "gap/DPO.gi.autogen.jl" );
